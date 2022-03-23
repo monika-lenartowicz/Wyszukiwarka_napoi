b8264fbb22c26d9da4f5db1dbcfbc78a115ea17a
Monika Lenartowicz <nika.nel@interia.pl>
2022-03-23 09:48:26 +0100

    Added script js with method to lower case

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const searchInput = document.querySelector(".search");
+const drinksList = document.querySelectorAll("li");
 
+const searchValue = e => {
+	const text = e.target.value.toLowerCase();
+
+	drinksList.forEach(element => {
+		const task = element.textContent;
+		if (task.toLowerCase().indexOf(text) !== -1) {
+			element.style.display = "block";
+		} else {
+			element.style.display = "none";
+		}
+	});
+};
+
+searchInput.addEventListener("keyup", searchValue);
